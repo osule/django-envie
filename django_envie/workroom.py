29f27301709bba0474337d9bba4d7bb175ef82e0
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-18 08:56:43 +0100

    Create workroom for converting vars

---
+++ django_envie/workroom.py
@@ -0,0 +1,20 @@
+from re import search
+import os
+
+
+def convertfiletovars():
+    """
+    Set environment variables from .env.py
+    if it exists in the project dir.
+    """
+    filepath = os.getcwd() + '/.env.py'
+    if os.path.isfile(filepath):
+        with open(filepath, 'r') as env_file:
+            for setting in env_file:
+                match = search("(\w+)\s=\s\"(.*?)\"", setting)
+                env_var, config = match.groups()
+                os.environ[env_var] = config
+
+if __name__ == '__main__':
+    convertfiletovars()
+    print "Yes"
