190ce1ac6f98db9bc2ba43ff6550c16b0b385e00
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-18 08:53:39 +0100

    Remove argument passed in convertfiletovars function

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django.test import TestCase
-from django_envie import convertfiletovars
+from django_envie.workroom import convertfiletovars
 
 import os
 
 
 class DjangoEnvieTestCase(TestCase):
     def test_convertfiletovars(self):
-        convertfiletovars('.env.py')
+        convertfiletovars()
         secret_key = os.getenv('SECRET_KEY')
         self.assertEqual(secret_key, 'Not so secret')
