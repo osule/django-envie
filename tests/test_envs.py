b7363eaad0e235cc6729d34459a202446cf853a1
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-28 14:27:38 +0100

    (fix) test that environment file outside of project can be seen

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -8,6 +8,20 @@ import os
 
 class DjangoEnvieTestCase(TestCase):
     def test_convertfiletovars(self):
+        filepath = os.path.join('.env.py')
+        with open(filepath, 'w+') as f:
+            f.write('SECRET_KEY = "Not so secret"')
         convertfiletovars()
         secret_key = os.getenv('SECRET_KEY')
         self.assertEqual(secret_key, 'Not so secret')
+        os.remove(filepath)
+
+    def test_convertfiletovars_env_file_outside_project(self):
+        filepath = os.path.join('..', '.env.py')
+        with open(filepath, 'w+') as f:
+            f.write('SECRET_KEY = "Not so secret"')
+        convertfiletovars()
+        secret_key = os.getenv('SECRET_KEY')
+        self.assertEqual(secret_key, 'Not so secret')
+        tmp_path = os.path.dirname(os.path.realpath(__file__))
+        os.remove(os.path.join(tmp_path, '..', '..', '.env.py'))
