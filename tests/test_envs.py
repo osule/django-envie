ac0229524977e588821ed5d22f5b2f97740d0f62
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-28 15:30:41 +0100

    Test that yml files can be read into environment

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -15,13 +15,14 @@ class DjangoEnvieTestCase(TestCase):
         secret_key = os.getenv('SECRET_KEY')
         self.assertEqual(secret_key, 'Not so secret')
         os.remove(filepath)
+        del os.environ['SECRET_KEY']
 
     def test_convertfiletovars_env_file_outside_project(self):
-        filepath = os.path.join('..', '.env.py')
+        filepath = os.path.join('..', '.env.yml')
         with open(filepath, 'w+') as f:
-            f.write('SECRET_KEY = "Not so secret"')
-        convertfiletovars()
-        secret_key = os.getenv('SECRET_KEY')
+            f.write('secret_key : "Not so secret"\r\nmy_name : "Oluwafemi"')
+        convertfiletovars(fileext='yml')
+        secret_key = os.getenv('secret_key')
         self.assertEqual(secret_key, 'Not so secret')
         tmp_path = os.path.dirname(os.path.realpath(__file__))
-        os.remove(os.path.join(tmp_path, '..', '..', '.env.py'))
+        os.remove(os.path.join(tmp_path, '..', '..', '.env.yml'))
