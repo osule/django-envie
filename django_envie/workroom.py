6fc821161d7a1ed12490827ae347833d0a3e4b5a
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-08 09:37:12 +0100

    Catching exceptions when environment file doesn't exist

--- django_envie/workroom.py
+++ django_envie/workroom.py
@@ -15,13 +15,21 @@ def convertfiletovars(fileext='py'):
         filepath = os.path.join('.env.'+fileext)
     if os.path.isfile(filepath):
         if fileext == 'py':
-            with open(filepath, 'r') as env_file:
-                for setting in env_file:
-                    match = search("(\w+)\s=\s\"(.*?)\"", setting)
-                    env_var, config = match.groups()
-                    os.environ[env_var] = config
+            try:
+                with open(filepath, 'r') as env_file:
+                    for setting in env_file:
+                        match = search("(\w+)\s=\s\"(.*?)\"", setting)
+                        env_var, config = match.groups()
+                        os.environ[env_var] = config
+            except:
+                print "No .env.py environment file found in your project directory.\
+                Go ahead and create one."
         elif fileext == 'yml':
             from yaml import load
-            environ_data = load(file(filepath, 'r'))
-            for key, value in environ_data.iteritems():
-                os.environ[key] = value
+            try:
+                environ_data = load(file(filepath, 'r'))
+                for key, value in environ_data.iteritems():
+                    os.environ[key] = value
+            except:
+                print "No .env.yaml environment file found in your project directory.\
+                Go ahead and create one."
