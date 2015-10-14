abbcaff622d6b6a96e4e6f6f81db3eda0c0dca7b
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-14 05:20:36 +0100

    (chore) update test

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django.test import TestCase
-from django_envie.workroom import convertfiletovars
+from django_envie.workroom import convertfiletovars, BASE_DIR
 
 import os
 
 
 class DjangoEnvieTestCase(TestCase):
-    def test_convertfiletovars(self):
+    def test_py_inside_project(self):
         filepath = os.path.join('.env.py')
         with open(filepath, 'w+') as f:
             f.write('SECRET_KEY = "Not so secret"')
@@ -17,12 +17,37 @@ class DjangoEnvieTestCase(TestCase):
         os.remove(filepath)
         del os.environ['SECRET_KEY']
 
-    def test_convertfiletovars_env_file_outside_project(self):
-        filepath = os.path.join('..', '.env.yml')
+    def test_yml_inside_project(self):
+        filepath = os.path.join('.env.yml')
         with open(filepath, 'w+') as f:
-            f.write('secret_key : "Not so secret"\r\nmy_name : "Oluwafemi"')
-        convertfiletovars(fileext='yml')
-        secret_key = os.getenv('secret_key')
+            f.write('SECRET_KEY : "Not so secret"')
+        convertfiletovars()
+        secret_key = os.getenv('SECRET_KEY')
         self.assertEqual(secret_key, 'Not so secret')
-        tmp_path = os.path.dirname(os.path.realpath(__file__))
-        os.remove(os.path.join(tmp_path, '..', '..', '.env.yml'))
+        os.remove(filepath)
+        del os.environ['SECRET_KEY']
+
+    def test_py_outside_project(self):
+        filepath = os.path.join(BASE_DIR, '..', '.env.py')
+        with open(filepath, 'w+') as f:
+            f.write('SECRET_KEY = "Not so secret"')
+        convertfiletovars()
+        secret_key = os.getenv('SECRET_KEY')
+        self.assertEqual(secret_key, 'Not so secret')
+        os.remove(filepath)
+        del os.environ['SECRET_KEY']
+
+    def test_yml_outside_project(self):
+        filepath = os.path.join(BASE_DIR, '..', '.env.yml')
+        with open(filepath, 'w+') as f:
+            f.write('SECRET_KEY : "Not so secret"')
+        convertfiletovars()
+        # import pdb; pdb.set_trace()
+        secret_key = os.getenv('SECRET_KEY')
+        self.assertEqual(secret_key, 'Not so secret')
+        os.remove(filepath)
+        del os.environ['SECRET_KEY']
+
+    def test_an_error_msg_when_no_yml(self):
+        res = convertfiletovars()
+        self.assertIn('No environment file', res)
