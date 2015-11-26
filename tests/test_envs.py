85e90006a3cfba3364c446dd83623bb3fab32ad9
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-26 14:15:57 +0100

    (chore) tests cover possible cases

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from django.test import TestCase
-from django_envie.workroom import convertfiletovars, BASE_DIR
+from django_envie.workroom import convertfiletovars, BASE_DIR, Config
 
 import os
 
 
 class DjangoEnvieTestCase(TestCase):
     def test_py_inside_project(self):
+        """Test that a config file inside of the project folder
+        with the .py extension is detected
+        """
         filepath = os.path.join('.env.py')
         with open(filepath, 'w+') as f:
             f.write('SECRET_KEY = "Not so secret"')
@@ -18,6 +21,9 @@ class DjangoEnvieTestCase(TestCase):
         del os.environ['SECRET_KEY']
 
     def test_yml_inside_project(self):
+        """Test that a config file inside the project folder
+        with the .yml extension is detected
+        """
         filepath = os.path.join('.env.yml')
         with open(filepath, 'w+') as f:
             f.write('SECRET_KEY : "Not so secret"')
@@ -28,6 +34,9 @@ class DjangoEnvieTestCase(TestCase):
         del os.environ['SECRET_KEY']
 
     def test_py_outside_project(self):
+        """Test that a config file outside of the project folder
+        with the .py extension is detected
+        """
         filepath = os.path.join(BASE_DIR, '..', '.env.py')
         with open(filepath, 'w+') as f:
             f.write('SECRET_KEY = "Not so secret"')
@@ -38,6 +47,9 @@ class DjangoEnvieTestCase(TestCase):
         del os.environ['SECRET_KEY']
 
     def test_yml_outside_project(self):
+        """Test that a config file outside of the project folder
+        with the .py extension is detected
+        """
         filepath = os.path.join(BASE_DIR, '..', '.env.yml')
         with open(filepath, 'w+') as f:
             f.write('SECRET_KEY : "Not so secret"')
@@ -48,6 +60,17 @@ class DjangoEnvieTestCase(TestCase):
         os.remove(filepath)
         del os.environ['SECRET_KEY']
 
-    def test_an_error_msg_when_no_yml(self):
+    def test_error_is_raised_when_no_config_file(self):
+        """Test that an error message is returned when a config
+        file isn't present
+        """
+        pyfilepath = os.path.join(BASE_DIR, '.env.py')
+        ymlfilepath = os.path.join(BASE_DIR, '.env.yml')
+        self.assertEqual(IOError, type(Config.parse_file(pyfilepath)))
+        self.assertEqual(IOError, type(Config.parse_file(ymlfilepath)))
+        pyfilepath = os.path.join(BASE_DIR, '..', '.env.py')
+        ymlfilepath = os.path.join(BASE_DIR, '..', '.env.yml')
+        self.assertEqual(IOError, type(Config.parse_file(pyfilepath)))
+        self.assertEqual(IOError, type(Config.parse_file(ymlfilepath)))
         res = convertfiletovars()
         self.assertIn('No environment file', res)
