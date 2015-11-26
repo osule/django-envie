2c2e534b227e4ebb1a3e57c9dbecc8efae45eeef
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-26 14:15:12 +0100

    (chore) refactor using Config class

--- django_envie/workroom.py
+++ django_envie/workroom.py
@@ -1,16 +1,52 @@
 from re import search
 import os
 
-
 BASE_DIR = os.getcwd()
 
-ERROR = """
-No environment file found in your project directory.
-Go ahead and create one.
-"""
 
-PYENV = '.env.py'
-YAMLENV = '.env.yml'
+class Config():
+    ERROR = """
+    No environment file found in your project directory.
+    Go ahead and create one.
+    """
+    PYFILEPATH = os.path.join(BASE_DIR, '.env.py')
+    YAMLFILEPATH = os.path.join(BASE_DIR, '.env.yml')
+
+    @staticmethod
+    def is_file(file_path):
+        """Checks that a file exists"""
+        return os.path.isfile(file_path)
+
+    @staticmethod
+    def up_one_level(file_ext):
+        """Goes up one level in the directory"""
+        return os.path.join(BASE_DIR, '..', file_ext)
+
+    @staticmethod
+    def parse_file(file_path):
+        """Parses file and sets environment key"""
+        file_extension = file_path.split('.')[-1]
+
+        if file_extension == 'py':
+            try:
+                pyfile = open(file_path, 'r')
+                for setting in pyfile.readlines():
+                    match = search("(\w+)\s=\s\"(.*?)\"", setting)
+                    key, value = match.groups()
+                    os.environ.update({key: value})
+                return True
+            except Exception as e:
+                return e
+
+        elif file_extension == 'yml':
+            from yaml import load
+            try:
+                environ_data = load(file(file_path, 'r'))
+                for key, value in environ_data.iteritems():
+                    os.environ.update({key: value})
+                return True
+            except Exception as e:
+                return e
 
 
 def convertfiletovars():
@@ -19,51 +55,25 @@ def convertfiletovars():
     if it exists in the project dir.
     """
     FLAG_ERROR = False
-    res = None
-    # check if env.py exists
-    pyfilepath = os.path.join(BASE_DIR, PYENV)
-    yamlfilepath = os.path.join(BASE_DIR, YAMLENV)
-    if os.path.isfile(pyfilepath):
-        res = parse_file(pyfilepath)
-    elif os.path.isfile(yamlfilepath):
-        res = parse_file(yamlfilepath)
-    else:
-        # Backtrack to root directory
-        pyfilepath = os.path.join(BASE_DIR, '..', PYENV)
-        is_pyfilepath = os.path.isfile(pyfilepath)
-        yamlfilepath = os.path.join(BASE_DIR, '..', YAMLENV)
-        is_yamlfilepath = os.path.isfile(yamlfilepath)
-        if is_pyfilepath:
-            res = parse_file(pyfilepath)
-        elif is_yamlfilepath:
-            res = parse_file(yamlfilepath)
-        elif not is_pyfilepath and not is_yamlfilepath:
-            FLAG_ERROR = True
+    FLAG_SUCCEED = False
 
-    if FLAG_ERROR is True:
-        return ERROR
-    if res:
-        return res.message
+    while not FLAG_ERROR:
+        if FLAG_SUCCEED is True:  # quick break out of loop
+            break
+        if Config.is_file(Config.PYFILEPATH):  # does a .env.py file exist
+            FLAG_SUCCEED = Config.parse_file(Config.PYFILEPATH)
+        elif Config.is_file(Config.YAMLFILEPATH):  # does a .env.yml file exist
+            FLAG_SUCCEED = Config.parse_file(Config.YAMLFILEPATH)
+        else:
+            pyfilepath = Config.up_one_level('.env.py')  # up one level
+            yamlfilepath = Config.up_one_level('.env.yml')  # up one level
+            if Config.is_file(pyfilepath):
+                FLAG_SUCCEED = Config.parse_file(pyfilepath)
+            elif Config.is_file(yamlfilepath):
+                FLAG_SUCCEED = Config.parse_file(yamlfilepath)
+            elif not Config.is_file(pyfilepath) and not Config.is_file(
+                 yamlfilepath):
+                FLAG_ERROR = True
 
-
-def parse_file(file_path):
-    file_extension = file_path.split('.')[-1]
-
-    if file_extension == 'py':
-        try:
-            pyfile = open(file_path, 'r')
-            for setting in pyfile.readlines():
-                match = search("(\w+)\s=\s\"(.*?)\"", setting)
-                key, value = match.groups()
-                os.environ.update({key: value})
-        except Exception as e:
-            return e
-
-    elif file_extension == 'yml':
-        from yaml import load
-        try:
-            environ_data = load(file(file_path, 'r'))
-            for key, value in environ_data.iteritems():
-                os.environ.update({key: value})
-        except Exception as e:
-            return e
+    if FLAG_ERROR:
+        return Config.ERROR
